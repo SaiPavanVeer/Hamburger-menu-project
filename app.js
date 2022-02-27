b9126659e58c554ead773974227f69d246737113
SaiPavanVeer <99801722+SaiPavanVeer@users.noreply.github.com>
2022-02-27 12:58:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const logo = document.querySelector('.navul');
+const menu = document.querySelector('.hamburger');
+menu.addEventListener('click',function(){
+    logo.classList.toggle('showmenu');
+});
+
